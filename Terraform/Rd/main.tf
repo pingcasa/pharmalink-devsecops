e22c93074baf729ab429b567d5db8944ae5082a7
unknown <hamza.agdid.emc2@ehtp.ac.ma>
2026-06-28 20:01:40 +0100

    migrate tfstate to azure blob storage

--- Terraform/Rd/main.tf
+++ Terraform/Rd/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatefile2026"
+    container_name       = "tfstate"
+    key                  = "rd-pipeline/terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
